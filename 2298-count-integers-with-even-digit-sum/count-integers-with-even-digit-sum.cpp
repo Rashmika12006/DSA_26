cb694cdeb55e084190b6d8235e321da0505bb21c
Rashmika Sharma <rashmikasharma913@gmail.com>
2026-09-22 22:12:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.59%) - LeetSync

---
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countEven(int num) {
+        int count = 0;
+      
+      for (int i = 1; i <= num; i++) 
+        {
+            int temp = i;
+            int sum = 0;
+
+            while (temp) 
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if (sum % 2 == 0) 
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
